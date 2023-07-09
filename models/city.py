bfadecb77fb6d57991f9499f6df9a8e5c41d6a5e
diegosmen111 <dbav_10@hotmail.com>
2023-07-08 17:38:52 -0700

    update city

--- models/city.py
+++ models/city.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
-
 """Defines the City class."""
-
 from models.base_model import BaseModel
 
 
-
-
-
 class City(BaseModel):
+    """Represent a city.
 
-        """Represent a city.
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
 
-
-
-            Attributes:
-
-                    state_id (str): The state id.
-
-                            name (str): The name of the city.
-
-                                """
-
-
-
-                                    state_id = ""
-
-                                        name = ""
+    state_id = ""
+    name = ""
